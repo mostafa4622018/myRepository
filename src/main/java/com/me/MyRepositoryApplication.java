f558b0ddd5564fffabb99432cc1e1b1e025e7840
mostafa4622018 <mostafaahmadi462@gmail.com>
2020-11-24 21:26:09 +0330

    first commit

---
+++ src/main/java/com/me/MyRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRepositoryApplication.class, args);
+    }
+
+}
